4025a094b00af1e06a3a898b6eeb335d2499d7da
alexis.renard <alexis.renard@ext.bpifrance.fr>
2020-11-03 13:54:18 +0100

    :wrench: Add EC2 output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 ##############################################################
 # Define layer outputs
 ##############################################################
+
+output "ec2_instance" {
+  description = "EC2 instance ARN"
+  value = aws_instance.web.arn
+}
