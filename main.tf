5d80b69f91d1932862ccc6d7dcb6002b8c81e44d
alexis.renard <alexis.renard@ext.bpifrance.fr>
2022-04-25 23:22:17 +0200

    :sparkles: Add example with S3 bucket (compliant and non compliant one with tfsec)

--- main.tf
+++ main.tf
@@ -2,6 +2,44 @@
 # S3 bucket example
 ###############################################################################
 
-resource "aws_s3_bucket" "demo" {
-  bucket = "pre-commit-demo"
+#===================================================
+# Non compliant bucket
+#===================================================
+
+resource "aws_s3_bucket" "non_compliant" {
+  bucket = "pre-commit-demo-s3-bucket-non-compliant"
 }
+
+#===================================================
+# Compliant bucket
+#===================================================
+
+# resource "aws_kms_key" "compliant" {
+#   description             = "This key is used to encrypt bucket objects"
+#   deletion_window_in_days = 10
+#   enable_key_rotation     = true
+# }
+
+# resource "aws_s3_bucket" "compliant" {
+#   bucket = "pre-commit-demo-s3-bucket-compliant"
+#   versioning {
+#     enabled = true
+#   }
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         kms_master_key_id = aws_kms_key.compliant.arn
+#         sse_algorithm     = "aws:kms"
+#       }
+#     }
+#   }
+# }
+
+# resource "aws_s3_bucket_public_access_block" "compliant" {
+#   bucket = aws_s3_bucket.compliant.id
+
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   restrict_public_buckets = true
+#   ignore_public_acls      = true
+# }
