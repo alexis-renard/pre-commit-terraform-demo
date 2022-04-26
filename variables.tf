686c8eeee52744db16a11a77d3e94279826614ff
alexis.renard <arenard@ippon.fr>
2022-04-26 12:50:25 +0200

    :fire: Remove useless local tag declaration in branch

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
 ##########################################
 # Define layer variables
 ##########################################
-
-locals {
-  tags = {
-    Application_Code = "DMO"
-    Owner            = "CCOE"
-    Cost_Center      = "Transverse"
-    Environment      = "DEV"
-    Name             = "DEMO INSTANCE"
-    Description      = "Please destroy if not done yet"
-  }
-}
